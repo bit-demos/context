a42552ab0dc2997ec9a5f4f13625ddeebd51649f
bengil <benjyg@gmail.com>
2021-11-16 13:27:08 +0200

    bring in api-hook-factory

---
+++ components/hooks/utils/api-hook-factory/api-hook-factory.composition.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { MemoryRouter } from 'react-router';
+import { Link } from '@learn-bit-react/base-ui.ui.link'
+
+// Link to example component which is a concrete implementation of this factory
+export const LinkToUseSearchMovie = () => {
+    return (
+        <MemoryRouter>
+
+            Please see the <Link href="https://bit.dev/learn-bit-react/context/hooks/use-search-movies" external>Use Search Movies component</Link> for
+            a full composition of this component.
+        </MemoryRouter>
+    )
+}
